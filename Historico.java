166bfa81278fa57c0134bfa65f0aa26a59c684cc
GiovaniLenoir <gionanilenoir@gmail.com>
2025-08-24 22:12:14 -0300

    Criado a classe Historico.java

---
+++ Historico.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Historico {
+    private ArrayList<String> registros;
+
+    public Historico() {
+        registros = new ArrayList<>();
+    }
+
+    public void adicionar(String operacao) {
+        registros.add(operacao);
+    }
+
+    public void exibir() {
+        System.out.println("\n=== HISTÓRICO ===");
+        if (registros.isEmpty()) {
+            System.out.println("Nenhuma operação realizada ainda.");
+        } else {
+            for (String registro : registros) {
+                System.out.println(registro);
+            }
+        }
+    }
+}
+
